4d94b6d13786ab02cbc761ff166d2a968bd2cbf1
Felipe-ABS <FelipeABS2@Gmail.com>
2022-07-04 18:59:08 -0300

    Lista de Heróis e Estilização Dinâmica

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: "Zenitsu"},
+  {id: 2, name: "Tanjiro"},
+  {id: 3, name: "Nezuko"},
+  {id: 4, name: "Inosuke"},
+  {id: 5, name: "Rengoku"},
+  {id: 6, name: "Shinobu"},
+  {id: 7, name: "Hanako-Kun"},
+  {id: 8, name: "Yashiro"},
+  {id: 9, name: "Gon"},
+  {id: 10, name: "Killua"},
+];
