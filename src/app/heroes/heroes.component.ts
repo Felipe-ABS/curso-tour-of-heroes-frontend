8e22574eb7461472c0ae6d5a76c44331b12b9413
Felipe-ABS <FelipeABS2@Gmail.com>
2022-07-05 17:04:19 -0300

    Transformando os dados do mock em dados Observáveis

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,7 +21,10 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    // O subscribe notifica quando houver uma mudança no Observable (dentro dele tem as ações do que será feito quando for notificado)
+    // O heroes é uma variável, ou meio que o parâmetro da função, ja que esta utilizando a "Arrow Function"
+    this.heroService.getHeroes().subscribe(heroes =>
+      this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
