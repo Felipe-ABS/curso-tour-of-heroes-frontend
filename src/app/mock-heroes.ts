8a259c9da060a7a9a94e3af76117f1cb4a8d0812
Felipe-ABS <FelipeABS2@Gmail.com>
2022-07-05 14:54:13 -0300

    Detalhe dos heróis

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -7,8 +7,8 @@ export const HEROES: Hero[] = [
   {id: 4, name: "Inosuke"},
   {id: 5, name: "Rengoku"},
   {id: 6, name: "Shinobu"},
-  {id: 7, name: "Hanako-Kun"},
-  {id: 8, name: "Yashiro"},
-  {id: 9, name: "Gon"},
-  {id: 10, name: "Killua"},
+  {id: 7, name: "Tengen"},
+  {id: 8, name: "Muichiro"},
+  {id: 9, name: "Giyu"},
+  {id: 10, name: "Obanai"},
 ];
