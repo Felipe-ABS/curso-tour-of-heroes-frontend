c497dd27dda0f3a94f2cf0b2ebf78151840a9647
Felipe-ABS <FelipeABS2@Gmail.com>
2022-07-07 17:41:13 -0300

    Adicionado Material e Modulo para guardar os imports dos modulos do Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
+//AutoImported
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Angular
 import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule } from '@angular/material/card';
 
+//Components
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -32,13 +30,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserAnimationsModule,
 
     FormsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatButtonModule,
-    MatExpansionModule,
-    MatIconModule,
     FlexLayoutModule,
-    MatCardModule
+    MaterialModule,
 
   ],
   providers: [],
